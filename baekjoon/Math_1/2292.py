3f6c376b3ce465f43d9853bc85798e5aac13ef5c
god9599 <59276134+god9599@users.noreply.github.com>
2021-01-07 20:05:57 +0900

    feat: 벌집

---
+++ baekjoon/Math_1/2292.py
@@ -0,0 +1,20 @@
+import sys
+
+dst = int(sys.stdin.readline())
+cnt = 1
+
+if(dst == 1):
+    cnt = 1
+else:
+    num = 0
+    while dst > num:
+         num += (cnt*6)
+         cnt += 1
+
+print(cnt)
+
+
+
+
+
+
