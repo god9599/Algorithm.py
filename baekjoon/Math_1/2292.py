6c97c65988db1df40a8eac93e033d2a8d7922448
god9599 <59276134+god9599@users.noreply.github.com>
2021-07-20 00:01:09 +0900

    Fix: 벌집

--- baekjoon/Math_1/2292.py
+++ baekjoon/Math_1/2292.py
@@ -3,18 +3,12 @@ import sys
 dst = int(sys.stdin.readline())
 cnt = 1
 
-if(dst == 1):
+if dst == 1:
     cnt = 1
 else:
-    num = 0
+    num = 1
     while dst > num:
-         num += (cnt*6)
-         cnt += 1
+        num += (cnt*6)
+        cnt += 1
 
 print(cnt)
-
-
-
-
-
-
