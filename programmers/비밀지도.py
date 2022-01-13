31a090efce310062443e602d6327d4799c2b97e4
god9599 <59276134+god9599@users.noreply.github.com>
2022-01-13 21:30:14 +0900

    Update 비밀 지도

--- programmers/비밀지도.py
+++ programmers/비밀지도.py
@@ -1,31 +1,16 @@
 def solution(n, arr1, arr2):
-    answer = []
-
-    dec1 = decrypt(n, arr1)
-    dec2 = decrypt(n, arr2)
-    for _dec1, _dec2 in zip(dec1, dec2):
-        result = ''
-        for j in range(n):
-            if _dec1[j] == 1 or _dec2[j] == 1:
-                result += '#'
-            elif _dec1[j] == 0 and _dec2[j] == 0:
-                result += ' '
-        answer.append(result)
-    return answer
-
-
-def decrypt(n, arr):
-    decrypt_list = []
-    for i in arr:
-        li = []
-        while True:
-            li.append(i % 2)
-            i = i // 2
-            if i == 0:
-                if len(li) < n:
-                    for i in range(1, n - len(li) + 1):
-                        li.append(0)
-                break
-        li.reverse()
-        decrypt_list.append(li)
-    return decrypt_list
+    li = []
+    for i, j in zip(arr1, arr2):
+        list1 = int(bin(i)[2:])
+        list2 = int(bin(j)[2:])
+        li.append(str(list1+list2).zfill(n))
+    fin = []
+    for a in li:
+        string = ''
+        for b in a:
+            if b == '0':
+                string += " "
+            else:
+                string += "#"
+        fin.append(string)
+    return fin
