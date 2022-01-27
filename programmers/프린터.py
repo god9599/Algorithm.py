ee2848959c53ac155bc7d7657eac8076e7af4566
god9599 <59276134+god9599@users.noreply.github.com>
2022-01-27 20:28:45 +0900

    프린터 - any

--- programmers/프린터.py
+++ programmers/프린터.py
@@ -1,27 +1,11 @@
-from collections import deque
-
-
 def solution(priorities, location):
-    queue = deque([(i, j) for i, j in enumerate(priorities)])
-    find = queue[location]
-    queue.reverse()
-    order = []
-
-    while queue:
-        a = _max(queue)
-        if queue[-1][1] < a:
-            queue.appendleft(queue[-1])
-            queue.pop()
+    queue = [(i, j) for i, j in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
         else:
-            order.append(queue[-1])
-            queue.pop()
-
-    return order.index(find) + 1
-
-
-def _max(li):
-    maxx = 0
-    for i in li:
-        if i[1] > maxx:
-            maxx = i[1]
-    return maxx
+            answer += 1
+            if cur[0] == location:
+                return answer
